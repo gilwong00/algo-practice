e9c703bd7545db78b7f30d703a90cb1db7df8ca0
Gilbert <gilwong3105@gmail.com>
2024-08-18 16:48:40 -0700

    Add descriptions

--- LongestPalindromicSubstring/main.go
+++ LongestPalindromicSubstring/main.go
@@ -1,5 +1,26 @@
 package main
 
+/**
+Given a string s, return the longest
+palindromic
+
+substring
+ in s.
+
+
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+
+*/
+
 func longestPalindrome(s string) string {
 	res := ""
 	length := len(s)
--- PalindromeNumber/main.go
+++ PalindromeNumber/main.go
@@ -1,5 +1,30 @@
 package main
 
+/**
+Given an integer x, return true if x is a
+palindrome
+, and false otherwise.
+
+
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+*/
+
 // /https://medium.com/@AlexanderObregon/solving-the-palindrome-number-on-leetcode-go-solutions-walkthrough-7951276b04e1
 func isPalindrome(x int) bool {
 	reversedInt := 0
